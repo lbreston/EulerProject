7527a2badef4613de6f03c6c0763e1efc3be8861
lbreston <leo.breston@gmail.com>
2021-11-15 14:18:30 -0800

    fixed floating point errors

--- P201-subsets-with-unique-sum.jl
+++ P201-subsets-with-unique-sum.jl
@@ -1,27 +1,49 @@
+# Signal processing library 
 using DSP
 
-#* Generating function for distribution of sums:  f(s) = ΣG(x,y) = Σg_n,k*x^n*y^k = ∏_n(1+x*y^k) 
-#* where n is the subset size and k is value of the sum 
+# Generating function for distribution of sums:  f(s) = ΣG(x,y) = Σg_n,k*x^n*y^k = ∏_n(1+x*y^k) 
+# where n is the subset size and k is value of the sum 
 
-#* n+1 x k+1 matrix encoding: row = size of subset, column = degree of value of sum
-#* first row and column represent n,k=0 
-function pfun(d,n)
-    z=zeros(Float64,2,d^2+1)
-    z[1,1]=1
-    z[2,n^2+1]=1
-    return z
+# creates vector of factors of generator function P[k] = (1+x*y^k) for sequence of squares from 1:k_max²
+function generating_factors(k_max)
+    #* n+1 x k+1 matrix encoding: row = size of subset, column = degree of value of sum
+    #* first row and column represent n,k=0 
+    function f̨ₖ(k)
+        f=zeros(Int8,2,k^2+1)
+        f[1,1]=1
+        f[2,k^2+1]=1
+        return f
+    end
+    return [f̨ₖ(k) for k in 1:k_max]
 end
 
-#* vector of factors of generator function P[k] = (1+x*y^k) 
-degree=100
-P=[pfun(degree,k) for k in 1:degree]
+# computes coefficients g_n_k for subsets of size n
+function compute_generating_coefs(G, n)
+    function clipvals(x)
+        x=abs(round(x))
+        if x==0
+            return 0
+        elseif x==1
+            return 1
+        else
+            return 2
+        end
+    end
+   # computes product using convolutions. Convolutions use FFTs to make it go way faster!
+    coefs = reduce((x,y)->clipvals.(conv(x,y)), G)
+    vals =  collect(0:size(coefs,2)-1)
+    return coefs[n+1, :], vals 
+end
 
-#* compute product using convolutions. Convolutions use FFTs to make it go way faster!
-G=reduce((x,y)->abs.(round.(conv(x,y))), P)
+# sum of the unique subset sums 
+function sum_unique_subset_sums(k_max, n)
+    G = generating_factors(k_max)
+    coefs, vals = compute_generating_coefs(G, n)
+    return sum(vals[coefs.==1])
+end
 
-#* Find sums of 50 element subsets that appear once i.e. g_51,k = 1 
-idx=findall(G[51,:].==1)
 
-#* Sum unique sums 
-sumval=0:1000001
-sum(sumval[idx])
+sum_unique_subset_sums(100,50)
+
+
+
